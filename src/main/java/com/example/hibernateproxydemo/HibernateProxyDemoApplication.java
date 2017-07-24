c0065c8d2d1e9d4130942c8afb0a8f2a5fa9f13c
Marcin Chwedczuk <chwedczukm@yahoo.com>
2017-07-24 19:02:23 +0200

    Application template generated by Spring Initializr

---
+++ src/main/java/com/example/hibernateproxydemo/HibernateProxyDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.hibernateproxydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateProxyDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateProxyDemoApplication.class, args);
+	}
+}
